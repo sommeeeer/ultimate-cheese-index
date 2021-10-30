d52d76362335fe7020d452262810fde261cf35dc
summeeeR <magnus@dahleide.com>
2021-10-30 18:43:07 +0200

    Add creator to Cheese model and migrations

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -27,6 +27,11 @@ class Cheese(TimeStampedModel):
     firmness = models.CharField('Firmness', max_length=20,
         choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
 
     def __str__(self):
         return self.name
